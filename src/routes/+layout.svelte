9998b035498052824ba1187c1182a02a225942da
James Camilleri <james@james.mt>
2023-05-29 21:07:24 +0200

    Add download button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+  import Download from '@fortawesome/fontawesome-free/svgs/solid/download.svg'
   import Moon from '@fortawesome/fontawesome-free/svgs/solid/moon.svg'
   import Play from '@fortawesome/fontawesome-free/svgs/solid/play.svg'
   import Sun from '@fortawesome/fontawesome-free/svgs/solid/sun.svg'
 
   import { darkMode, playing } from './config'
+
+  let link: HTMLAnchorElement
+  let downloads = 0
 
   function toggle() {
     $darkMode = !$darkMode
@@ -11,6 +15,28 @@
 
   function play() {
     $playing = true
+  }
+
+  function download() {
+    const svg = document.getElementById('svg')
+
+    if (!svg || !link) return
+
+    const svgText = svg.outerHTML
+      .replaceAll('style="d: path(&quot;', 'd="')
+      .replaceAll('&quot;);', '')
+      .replaceAll(/class=".*?"/g, '')
+      .replaceAll(/var\(--foreground\)/g, '#000000')
+      .replaceAll(/var\(--background\)/g, '#ffffff')
+
+    downloads++
+    const blob = new Blob([svgText], { type: 'image/svg+xml' })
+    const url = window.URL.createObjectURL(blob)
+
+    link.target = '_blank'
+    link.download = `generated_${downloads}.svg`
+    link.href = url
+    link.click()
   }
 </script>
 
@@ -27,11 +53,14 @@
   </div>
 {/if}
 <slot />
+<div class="download icon" on:click={download} style:color={$darkMode ? 'rgb(255 255 255)' : 'rgb(0 0 0)'}>
+  <Download />
+</div>
+<a class="link" bind:this={link} />
 
 <style>
   .icon {
     position: fixed;
-    top: 1.5em;
     width: 1.5em;
     height: 1.5em;
     color: rgb(255 255 255);
@@ -39,10 +68,21 @@
   }
 
   .dark-toggle {
+    top: 1.5em;
     right: 2em;
   }
 
   .play {
+    top: 1.5em;
     right: 5em;
+  }
+
+  .download {
+    right: 2em;
+    bottom: 1.5em;
+  }
+
+  .link {
+    display: none;
   }
 </style>
