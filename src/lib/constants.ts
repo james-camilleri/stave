8638e4af10330babe532024a070568b4bfcdd44a
James Camilleri <james@james.mt>
2023-12-21 16:22:44 +0100

    Add new constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,7 @@
 export const DARK = '#000000'
 export const LIGHT = '#ffffff'
-
 export const PINK = '#eb5c82'
 export const ORANGE = '#f37e12'
+
+export const FILL_OPACITY = 0.1
+export const PATTERN_RANDOM_OFFSET = 3
